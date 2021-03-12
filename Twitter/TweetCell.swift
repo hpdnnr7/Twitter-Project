7fe477b6df582f8bf9c0eaca6fff38d70f1b40fa
hpdnnr7 <hpdnnr7@gmail.com>
2021-03-11 22:53:48 -0500

    final

---
+++ Twitter/TweetCell.swift
@@ -0,0 +1,24 @@
+//
+//  TweetCell.swift
+//  Twitter
+//
+//  Created by Hope Dunner on 3/4/21.
+//  Copyright © 2021 Dan. All rights reserved.
+//
+
+import UIKit
+
+class TweetCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
---
+++ Twitter/TweetCellTableViewCell.swift
@@ -0,0 +1,69 @@
+//
+//  TweetCellTableViewCell.swift
+//  Twitter
+//
+//  Created by Hope Dunner on 3/4/21.
+//  Copyright © 2021 Dan. All rights reserved.
+//
+
+import UIKit
+
+class TweetCellTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+    
+    @IBOutlet weak var profileImageView: UIImageView!
+    @IBOutlet weak var userNameLabel: UILabel!
+    @IBOutlet weak var tweetContent: UILabel!
+    
+    
+    @IBOutlet weak var retweetButton: UIButton!
+    
+    
+    @IBOutlet weak var favButton: UIButton!
+    
+    var favorited:Bool = false
+    var tweetId:Int = -1
+    
+    @IBAction func favoriteTweet(_ sender: Any) {
+        let toBeFavorited = !favorited
+        if (toBeFavorited){
+            TwitterAPICaller.client?.favoriteTweet(tweetId: tweetId, success: {
+                self.setFavorite(true), failure: { (error) in
+                    print("Favorite did not succeed: \(error)")
+                })
+            } else {
+            TwitterAPICaller.client?.unfavoriteTweet(tweetId: tweetId, success: {
+            self.setFavorite(true), failure: { (error) in
+                print("Unfavorite did not succeed: \(error)")
+            
+            })
+            }
+        
+    }
+    
+    @IBAction func retweet(_ sender: Any) {
+    }
+    
+    
+    
+    func setFavorite(_ isFavorited:Bool) {
+        favorited = isFavorited
+        if(favorited) {
+            favButton.setImage(UIImage(named: "favor-icon-red"), for: UIControl.State.normal)
+        }
+        else{
+            
+            favButton.setImage(UIImage(named: "favor-icon"), for: UIControl.State.normal)
+    }
+}
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
